1b387b0db5e6ad66c997e23c8b51f02c7d86a6f2
Maël Gangloff <contact@maelgangloff.fr>
2023-04-17 15:24:20 +0200

    fix: add attributes EvaluationDetail

--- src/models/EvaluationDetail.ts
+++ src/models/EvaluationDetail.ts
@@ -4,14 +4,14 @@ export interface EvaluationDetail {
     id: string;
     type: string;
     attributes: {
-        title: string;
-        topic: string;
-        dateTime: string;
+        title: string|null;
+        topic: string|null;
+        dateTime: string|null;
         coefficient: number;
         min: number|null;
         max: number|null;
         average: number|null;
-        scale: number;
+        scale: number|null;
     };
     relationships: {
         subSkills: {
@@ -42,10 +42,15 @@ export interface EvaluationDetail {
 }
 
 export type EvaluationDetailIncluded = Included<Partial<{
-    mark: number;
+    mark: number|null;
     nonEvaluationReason: null;
     comment: null;
     label: string;
+    coefficient: number|null
+    average: number|null
+    scale: number|null
+    studentAverage: number|null
+    photoUrl: string|null
     color: string;
     title: string;
     firstName: string;
