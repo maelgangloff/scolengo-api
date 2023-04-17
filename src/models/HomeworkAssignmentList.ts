fd391269a72c3023788dc49c1372e7c2843f0263
NonozgYtb <nonozg.ytb@gmail.com>
2023-04-17 23:01:31 +0200

    feat(homework): add homework types

---
+++ src/models/HomeworkAssignmentList.ts
@@ -0,0 +1,49 @@
+import { Included } from './Globals'
+
+export interface HomeworkAssignmentList {
+    id: string;
+    type: string;
+    attributes: {
+        title: string;
+        html: string;
+        dueDateTime: string;
+        dueDate: string|null;
+        done: boolean;
+        deliverWorkOnline: boolean;
+        onlineDeliveryUrl: null|string;
+    };
+    relationships: {
+        teacher: {
+            data: {
+                id: string;
+                type: string;
+            };
+        };
+        attachments: {
+            data: {
+                id: string;
+                type: string;
+            }[];
+        };
+        subject: {
+            data: {
+                id: string;
+                type: string;
+            };
+        };
+    }
+}
+
+type Subject = {
+    label: string;
+    color: string;
+}
+
+type Teacher = {
+    title: string;
+    firstName: string;
+    lastName: string;
+    photoUrl: string|null;
+}
+
+export type HomeworkAssignmentListIncluded = Included<Subject|Teacher>
