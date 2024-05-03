4fb4e5ff8c50ccf3acb134847a6178d8f75874d1
Maël Gangloff <contact@maelgangloff.fr>
2024-05-03 23:52:00 +0200

    test: add unit tests

--- test/user.test.ts
+++ test/user.test.ts
@@ -1,6 +1,7 @@
 import { describe, expect } from '@jest/globals'
 import type { AuthConfig } from '../src/models/Common/Auth'
-import type { EvaluationSettings } from '../src/models/Results'
+import type { Evaluation, EvaluationSettings } from '../src/models/Results'
+import type { Agenda } from '../src/models/Calendar'
 import { Skolengo } from '../src/index'
 import './common'
 
@@ -12,21 +13,17 @@ const describeAuthenticated = SKOLENGO_TOKENSET !== undefined ? describe : descr
  */
 describeAuthenticated('Test of the Skolengo API types - Authenticated user', () => {
   let user: Skolengo
-  let userPermissions: string[] = []
 
   beforeAll(async () => {
     user = await Skolengo.fromConfigObject((JSON.parse(SKOLENGO_TOKENSET as string) as AuthConfig), { handlePronoteError: true })
-    const userInfo = await user.getUserInfo()
-    if (userInfo.permissions === undefined) throw new Error("Impossible de lister les habilitations de l'utilisateur.")
-    userPermissions = userInfo.permissions.map(p => p.permittedOperations).flat()
   })
 
-  it('should getUserInfo return User type', async () => {
+  it('should match type User', async () => {
     const response = await user.getUserInfo()
     expect(response).toMatchSchema('User')
   })
 
-  it('should getSchoolInfos return an array of SchoolInfo type', async () => {
+  it('should match type SchoolInfo[]', async () => {
     const response = await user.getSchoolInfos()
     for (const info of response) expect(info).toMatchSchema('SchoolInfo')
   })
@@ -35,34 +32,52 @@ describeAuthenticated('Test of the Skolengo API types - Authenticated user', () 
    * Tests unitaires du module d'évaluation
    */
   describe('Test of the Evaluation module', () => {
-    if (!userPermissions.includes('READ_EVALUATIONS')) return
-
     let evaluationSettingsList: EvaluationSettings[]
+    let evaluationList: Evaluation[]
 
     beforeAll(async () => {
       evaluationSettingsList = await user.getEvaluationSettings()
+      if (evaluationSettingsList.length === 0 || evaluationSettingsList[0].periods.length === 0) return
+
+      evaluationList = await user.getEvaluation(undefined, evaluationSettingsList[0].periods[0].id, 2)
     })
 
-    it('should getEvaluationSettings return EvaluationSettings type', async () => {
+    it('should match type EvaluationSettings', async () => {
       for (const evaluationSettings of evaluationSettingsList) expect(evaluationSettings).toMatchSchema('EvaluationSettings')
     })
 
-    it('should getEvaluation return an array of Evaluation', async () => {
-      if (evaluationSettingsList.length === 0 || evaluationSettingsList[0].periods.length === 0) return
-      const response = await user.getEvaluation(undefined, evaluationSettingsList[0].periods[0].id)
-      for (const evaluation of response) expect(evaluation).toMatchSchema('Evaluation')
+    it('should match type Evaluation[]', async () => {
+      for (const evaluation of evaluationList) expect(evaluation).toMatchSchema('Evaluation')
+    })
+
+    it('should match type EvaluationDetail[]', async () => {
+      for (const evaluation of evaluationList) {
+        const evaluationDetail = await user.getEvaluationDetail(undefined, evaluation.evaluations[0].id)
+        expect(evaluationDetail).toMatchSchema('EvaluationDetail')
+      }
     })
   })
 
   /**
-   * Tests du module de messagerie
+   * Tests unitaires du module de messagerie
    */
-  describe('Test of the Messagerie module', () => {
-    if (!userPermissions.includes('READ_MESSAGES')) return
-
-    it('should getUsersMailSettings return UsersMailSettings type', async () => {
+  describe('Test of the MSG module', () => {
+    it('should match type UsersMailSettings', async () => {
       const response = await user.getUsersMailSettings()
       expect(response).toMatchSchema('UsersMailSettings')
+    })
+  })
+
+  describe('Test of the CDT module', () => {
+    let agendaList: Agenda[]
+
+    beforeAll(async () => {
+      const dateToString = (date = new Date()): string => date.toISOString().split('T')[0]
+      agendaList = await user.getAgenda(undefined, dateToString(new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1e3)), dateToString(), 2)
+    })
+
+    it('should match type Agenda[]', async () => {
+      for (const agenda of agendaList) expect(agenda).toMatchSchema('Agenda')
     })
   })
 })
