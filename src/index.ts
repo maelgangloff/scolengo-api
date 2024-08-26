5d7426b80b933a2308c475819ffb6550fc846839
NonozgYtb <nonozg.ytb@gmail.com>
2024-08-26 02:11:36 +0200

    chore: scheiße i'm tired of commiting over and over for just these stupids tests

--- src/index.ts
+++ src/index.ts
@@ -1057,7 +1057,7 @@ export class Skolengo {
    */
   public getTokenClaims (): IdTokenClaims {
     if (this.tokenSet.id_token === undefined) throw new TypeError('id_token not present in TokenSet')
-    return JSON.parse(atob((this.tokenSet.id_token).split('.')?.at(1)?.replace(/-/g, '+').replace(/_/g, '/') || ''))
+    return JSON.parse(atob((this.tokenSet.id_token).split('.')?.at(1)?.replace(/-/g, '+').replace(/_/g, '/') ?? ''))
   }
 
   /**
